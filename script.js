a548eddde1c2f78a44cb85826306d8e5fdd99df0
Aditya Pandey <adityacoderr@gmail.com>
2023-11-30 20:07:06 +0530

    nothing added

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var sidebar = document.querySelector('.sidebar');
+//     var sidebar2 = document.querySelector('.sidebar2');
+//     var hamburger = document.querySelector('.hamburger');
+
+
+//     hamburger.addEventListener('click', function () {
+//         toggleSidebar(sidebar, sidebar2);
+//     });
+
+//     function toggleSidebar(sidebarToShow, sidebarToHide) {
+//         if (sidebarToHide.style.display === 'block') {
+//             sidebarToHide.style.display = 'none';
+//             sidebarToShow.style.display = 'block';
+
+//         } else {
+//             sidebarToHide.style.display = 'block';
+//             sidebarToShow.style.display = 'none';
+//         }
+//     }
+
+// });
